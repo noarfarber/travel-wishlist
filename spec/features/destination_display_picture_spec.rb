7b2f050a331d1cf9734b18f63aee591a9d9472d4
noarfarber <noaros88@gmail.com>
2021-12-29 18:43:45 +0000

    fixed a failing test

--- spec/features/destination_display_picture_spec.rb
+++ spec/features/destination_display_picture_spec.rb
@@ -1,13 +1,14 @@
 require 'rails_helper'
 
-feature 'Shows a picture of destination' do
-  scenario 'the user can view a picture of the destination' do
+feature 'Shows a picture of the destination' do
+  scenario 'The user can view a picture on the destination page' do
     visit '/destinations'
     click_link 'New destination'
     fill_in 'Location', with: 'Paris'
     attach_file 'Image', '/Users/noafarber/Projects/travel-wishlist/spec/features/images/something_img.png'
     click_button 'Create Destination'
     visit '/destinations/1'
+
     expect(page).to have_css("img[src*='something_img.png']")
   end
 end
--- spec/features/user_can_add_destination_spec.rb
+++ spec/features/user_can_add_destination_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
-RSpec.feature 'Destinations' do
+feature 'Destinations' do
   scenario 'A user can add a destination a see it' do
     visit '/destinations/new'
     fill_in 'destination_location', with: 'Paris'
+    attach_file 'Image', '/Users/noafarber/Projects/travel-wishlist/spec/features/images/something_img.png'
     click_button 'Create Destination'
-    visit '/destinations'
+    visit '/destinations/1'
 
     expect(page).to have_content 'Paris'
   end
